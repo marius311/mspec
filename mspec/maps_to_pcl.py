2a3d076390629e4f458691b78baff93ffffb0f8d
marius <mmillea@ucdavis.edu>
2012-02-25 19:40:28 -0800

    maps freqs

--- mspec/maps_to_pcl.py
+++ mspec/maps_to_pcl.py
@@ -17,9 +17,9 @@ if __name__=="__main__":
     # Read map file names
     regex = re.compile(params.get("map_regex",def_map_regex))
     maps = [(os.path.join(params["maps"],f),regex.search(f)) for f in os.listdir(params["maps"]) if ".fits" in f]
-    maps = sorted([(f,[r.group(1),r.group(2)]) for (f,r) in maps if r and (not params.get('freqs') or r.group(1) in params['freqs'])])
-    print 'Transforming the following maps:'+str([f for (f,_) in maps])
-    
+    maps = sorted([(f,[r.group(1),r.group(2)]) for (f,r) in maps if r and ('freqs' not in params or r.group(1) in params['freqs'])])
+    print 'Found the following maps: '+str([MapID(fr,'T',id) for (_,(fr,id)) in maps])
+     
     # Other options
     mask = H.read_map(params["mask"]) if params.get("mask") else None
     lmax = params["lmax"]
